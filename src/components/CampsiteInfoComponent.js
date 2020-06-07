818d7ae82c0870090bde32b6ffc4fce96de5646b
Margaret <margaretmhanken@gmail.com>
2020-06-06 21:09:15 -0700

    Adding to the Single Page App

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,55 +7,54 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 //     this.state = {};
 //   }
 
-function RenderComments({comments}) {
-    if (comments) {
-      return (
-        <div className="col-md-5 and m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => (
-            <div key={comment.id}>
-              {console.log("the comment is - ", comment)}
-              {comment.text} <br /> {comment.author}{" "}
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
-            </div>
-          ))}
-        </div>
-      );
-    }
-    return <div></div>;
-  }
-
-function RenderCampsite({campsite}) {
+function RenderComments({ comments }) {
+  if (comments) {
     return (
       <div className="col-md-5 and m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
+        <h4>Comments</h4>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            {console.log("the comment is - ", comment)}
+            {comment.text} <br /> {comment.author}{" "}
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(comment.date)))}
+          </div>
+        ))}
       </div>
     );
   }
+  return <div></div>;
+}
 
-  function CampsiteInfo(props) {
-    if (props.campsite) {
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(props.campsite)}
-            {this.renderComments(props.campsite.comments)}
-          </div>
+function RenderCampsite({ campsite }) {
+  return (
+    <div className="col-md-5 and m-1">
+      <Card>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardTitle>{campsite.name}</CardTitle>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function CampsiteInfo(props) {
+  if (props.campsite) {
+    return (
+      <div className="container">
+        <div className="row">
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments comments={props.campsite.comments} />
         </div>
-      );
-    }
-    return <div />;
+      </div>
+    );
   }
+  return <div />;
 }
 
 export default CampsiteInfo;
