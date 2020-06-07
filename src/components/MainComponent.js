fe9e7ab39ac11a7e4e35029dc97ec2b3703097b1
Margaret <margaretmhanken@gmail.com>
2020-06-06 21:55:07 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import { CAMPSITES } from "../shared/campsites_OLDDDD";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Contact from "./ContactComponent";
+import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
@@ -37,10 +37,26 @@ class Main extends Component {
       );
     };
 
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route path="/home" component={HomePage} />
           <Route
             exact
@@ -50,7 +66,7 @@ class Main extends Component {
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
-        <Directory
+        {/* <Directory
           campsites={this.state.campsites}
           onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
         />
@@ -60,13 +76,8 @@ class Main extends Component {
               (campsite) => campsite.id === this.state.selectedCampsite
             )[0]
           }
-        />
+        /> */}
         <Footer />
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
       </div>
     );
   }
